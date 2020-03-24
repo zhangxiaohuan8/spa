fa8b7cafe5c5fbbd5b62fa349be1ce28115a07ee
zhangxiaohuan8 <2916244782@qq.com>
2020-03-24 17:10:53 +0800

    add

--- 41-timer-button/timer-button.js
+++ 41-timer-button/timer-button.js
@@ -1,48 +1,9 @@
-// var $timerButton=(function(){
-//     var $btn=$('<input type="button" disabled/>'),
-//         cfg={
-//             container:'body',
-//             num:6,
-//             title:'同意',
-//         },
-//         num,
-//         timer;
-//     $btn.css({
-//         height:'50px',
-//         width:'100px'
-//     })
-//     function show(conf){
-//         // 1.DOM draw
-//         $(conf.container).append($btn);
-//         $.extend(cfg,conf);
-//         num=cfg.num;
-//         $btn.val(cfg.title+'('+cfg.num+'s)');
-//         timer=setInterval(function(){
-//             num--;
-//             if(cfg.num===0){
-//                 clearInterval(timer);
-//                 $btn.val(cfg.title);
-//                 $btn.removeAttr("disabled");
-//             }else{
-//                 $btn.val(cfg.title+'('+num+'s)')
-//             }
-//         },1000)
-//         // 2.event bind
-//     }
-//      $btn.click(function(){
-//         cfg.onClick();
-//      })
-//      return{
-//          show:show
-//      }
-// }())
 var $timerButton=(function(){
-    var $btn=$('<input type="button" value="同意（6s）" disabled/>'),
+    var $btn=$('<input id="btn" type="button" value="同意（6s）" disabled/>'),
         cfg = {
-            container:'body',
+            container:'div.main',
             num:6,
-            title:'同意',
-            // onClick:
+            title:'同意'
         },
         num,
         timer;
@@ -55,19 +16,23 @@ var $timerButton=(function(){
         $(cfg.container).append($btn);
         $.extend(cfg,conf);
         num = cfg.num;
-        $btn.val(cfg.title+'('+cfg.num+'s)')
-        timer=setInterval(function(){
-            num--;
-            if(num===0){
-                clearInterval(timer);
-                $btn.val(cfg.title);
-                $btn.removeAttr("disabled");
-            }else{
+        if(cfg.enable==true){
+            $btn.val(cfg.title);
+            $btn.removeAttr("disabled");
+        }
+        else{
+            $btn.val(cfg.title+'('+cfg.num+'s)')
+            timer=setInterval(function(){
+                num--;
+                if(num===0){
+                    clearInterval(timer);
+                    $btn.val(cfg.title);
+                    $btn.removeAttr("disabled");
+                }else{
                 $btn.val(cfg.title+'('+num+'s)')
-            }
-        },1000)
-        // $(conf).append($btn);
-        // 2.event bind
+                }
+            },1000)
+        }
     }
      $btn.click(function(){
         cfg.onClick();
