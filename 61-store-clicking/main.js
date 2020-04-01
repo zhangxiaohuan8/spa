fb8a2d5d4a6eeafd571573fcaf4960a6e1a6bf40
zhangxiaohuan8 <2916244782@qq.com>
2020-04-01 09:50:33 +0800

    61

---
+++ 61-store-clicking/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    var n = localStorage.getItem("n") || 0;
+    function t(n) {
+        $(".main input").val("被按了 " + n + " 次")
+    }
+    t(n),
+    $(".main input").click(function() {
+        localStorage.setItem("n", ++n),
+        t(n)
+    })
+});
